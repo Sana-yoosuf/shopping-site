e5b2e2d91f78a397d2b65d9834ad6bab1c1c0d81
Sana Yoosuf <sanazaier777@gmail.com>
2023-10-07 21:23:12 +0530

    design for navbar

--- clientview/src/App.js
+++ clientview/src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop/Shop';
 
 function App() {
   return (
     <div >
-   <Navbar/>
+
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ clientview/src/Pages/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const LoginSignup = () => {
+    return (
+        <div>Login or signup</div>
+    )
+}
+
+export default LoginSignup
